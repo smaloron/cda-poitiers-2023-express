5963a589ad8220409086df9a7a3c38c9d9c171b5
seb <contact@smaloron.eu>
2023-02-24 10:21:03 +0100

    middlewares

--- app.js
+++ app.js
@@ -10,15 +10,51 @@ const express = require('express');
 // Création de l'application
 const app = express();
 
+// Import du module morgan pour loguer les requêtes
+// et utilisation de ce module dans un middleware
+app.use(require('morgan')('tiny'));
+
+// Gestion des ressources statiques
+app.use('/img', express.static('images'));
+
+// Middleware perso
+app.use((req, res, next) => {
+    req.now = new Date().toLocaleDateString();
+    next();
+});
+
+// Gestion des données postées dans la requête HTTP
+// Récupération depuis un appel ajax (ex: axios)
+app.use(express.json());
+// Récupération depuis formulaire web ou Postman
+app.use(express.urlencoded({ extended: true }));
+
 // Définition d'une route
 app.get('/hello/:name', function (request, response) {
-    response.json({ message: `hello ${request.params.name}` });
+    response.json({
+        message: `hello ${request.params.name}`,
+        date: request.now
+    });
 });
 
 app.get('/addition/:n1([0-9]+)/:n2([0-9]+)', function (req, res) {
     res.json({
-        resultat: parseInt(req.params.n1) + parseInt(req.params.n2)
+        resultat: parseInt(req.params.n1) + parseInt(req.params.n2),
+        date: req.now
     });
+});
+/**
+ * test de la réception de données postées
+ */
+app.post('/test', (req, res) => {
+    res.json({
+        data: req.body
+    });
+});
+
+// Dernière route qui capture toute route non encore capturée
+app.all('*', (req, res) => {
+    res.status(404).json({ message: 'ressource introuvable' });
 });
 
 
