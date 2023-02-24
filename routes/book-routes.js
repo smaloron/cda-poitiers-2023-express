4794fa5df3a97598f6f75fb1e6976305dae11432
seb <contact@smaloron.eu>
2023-02-24 15:41:50 +0100

    mongoose find et findById

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -1,10 +1,23 @@
 const router = require('express').Router();
 const Book = require('../models/book-model');
 
+// Insertion de données
 router.post('/', async (req, res) => {
     const book = new Book.model(req.body);
     await book.save();
     res.json({ newBook: book });
+});
+
+// Affichage de toutes les données
+router.get('/', async (req, res) => {
+    const data = await Book.model.find();
+    res.json({ booklist: data });
+});
+
+// Affichage d'un livre
+router.get('/:id', async (req, res) => {
+    const data = await Book.model.findById(req.params.id);
+    res.json({ book: data });
 });
 
 
